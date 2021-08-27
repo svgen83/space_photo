59103405591a73a07777de0b7faa1d9ee337d81a
svg83 <svgeneraloff@gmail.com>
2021-08-27 15:22:30 +0400

    Update main.py
    исправил код для подготовки к изменению данных, поправил опечатки

--- main.py
+++ main.py
@@ -9,11 +9,11 @@ import requests
 from dotenv import load_dotenv
 
 
-def make_directory(path_name):
-    pathlib.Path(path_name).mkdir(parents=True, exist_ok=True)
+def make_directory(image_folder):
+    pathlib.Path(image_folder).mkdir(parents=True, exist_ok=True)
 
 
-def download_image(url, file_name, params = None):
+def download_image(url, file_name, params=None):
     response = requests.get(url, params)
     response.raise_for_status()
     with open(file_name, "wb") as file:
@@ -28,47 +28,50 @@ def get_extension(url):
     return extension
 
 
-def fetch_spacex_lunch(flight_number):
+def fetch_spacex_lunch(flight_number, image_folder):
     spacex_url = "https://api.spacexdata.com/v4/launches/"
-    spacex_template = "images/images_SPACEX/spacex{}.jpg"
+    
     response = requests.get(spacex_url)
     response.raise_for_status()
     links = response.json()[flight_number]["links"]["flickr"]["original"]
+
     for filenumber, link in enumerate(links):
-        file_name = spacex_template.format(filenumber)
+        file_name = (image_folder + "{}{}{}").format("spacex",filenumber,".jpg")
         download_image(link, file_name)
 
 
-def fetch_nasa_apod(token, images_quantity):
+def fetch_nasa_apod(token, image_folder, images_quantity):
     nasa_endpoint = "https://api.nasa.gov/planetary/apod"
-    nasa_apod_template = "images/images_NASA/{title}{ext}"
     params = {"api_key": token, "count": images_quantity}
+    
     response = requests.get(nasa_endpoint, params=params)
     response.raise_for_status()
     image_descriptions = response.json()
+
     for image_description in image_descriptions:
         url = image_description["url"]
         title = image_description["title"]
         ext = get_extension(url)
-        file_name = nasa_apod_template.format(title=title, ext=ext)
+        file_name = (image_folder + "{title}{ext}").format(title=title, ext=ext)
         download_image(url, file_name)
 
 
-def fetch_nasa_epic(token):
+def fetch_nasa_epic(token, image_folder):
     epic_endpoint = "https://api.nasa.gov/EPIC/api/natural/images"
-    nasa_epic_template = "images/images_EPIC/{title}.png"
     params = {"api_key": token}
+    
     response = requests.get(epic_endpoint, params=params)
     response.raise_for_status()
     image_descriptions = response.json()
     image_endpoint = "https://api.nasa.gov/EPIC/archive/natural/{file_date}/png/{title}.png"
+
     for image_description in image_descriptions:
         title = image_description["image"]
         image_date = image_description["date"]
         date_time = datetime.datetime.fromisoformat(image_date)
         file_date = date_time.strftime("%Y/%m/%d")
         url = image_endpoint.format(file_date=file_date, title=title)
-        file_name = nasa_epic_template.format(title=title)
+        file_name = (image_folder + "{0}{1}").format(title, ".png")
         download_image(url, file_name, params=params)
 
 
@@ -78,10 +81,14 @@ if __name__ == "__main__":
     
     flight_number = random.randint(1,157)
     image_quantity = 30
+    
+    spacex_folder = "./images/images_SPACEX/"
+    nasa_folder = "./images/images_NASA/"
+    epic_folder = "./images/images_EPIC/"
 
-    make_directory("./images/images_SPACEX")
-    fetch_spacex_lunch(flight_number)
-    make_directory("./images/images_NASA")
-    fetch_nasa_apod(token,image_quantity)
-    make_directory("./images/images_EPIC")
-    fetch_nasa_epic(token)
+    make_directory(spacex_folder)
+    fetch_spacex_lunch(flight_number, spacex_folder)
+    make_directory(nasa_folder)
+    fetch_nasa_apod(token,nasa_folder, image_quantity)
+    make_directory(epic_folder)
+    fetch_nasa_epic(token, epic_folder)
